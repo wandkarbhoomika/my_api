631b600d8a7788eb655291f96b566e58d90be06d
wandkarbhoomika <wandkarbhoomika@gmail.com>
2025-12-14 00:08:10 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "Public IP of EC2 instance"
-  value       = aws_instance.app.public_ip
-}
-
-output "s3_bucket_name" {
-  description = "S3 bucket for quote logs"
-  value       = aws_s3_bucket.quote_logs.bucket
-}
